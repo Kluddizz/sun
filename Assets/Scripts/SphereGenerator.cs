e7948cbe412f3f4ca38af74634e382d934b5832d
Kluddizz <flhansen.93@gmail.com>
2021-01-04 16:34:51 +0100

    Implement gizmos for sun surface.

--- Assets/Scripts/SphereGenerator.cs
+++ Assets/Scripts/SphereGenerator.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class SphereGenerator : MonoBehaviour
 {
+  public int recursionLevel = 1;
+  public float radius = 1.0f;
+  public Material material;
 
-    public int recursionLevel = 1;
-    public float radius = 1.0f;
-    public Material material;
+  void Start()
+  {
+    // Create components to be able to render the icosphere mesh.
+    MeshFilter filter = gameObject.AddComponent<MeshFilter>();
+    MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
 
-    void Start()
-    {
-        // Create components to be able to render the icosphere mesh.
-        MeshFilter filter = gameObject.AddComponent<MeshFilter>();
-        MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+    // Generate the mesh.
+    filter.sharedMesh = MeshGenerator.GenerateIcoSphereMesh(recursionLevel, radius);
 
-        // Generate the mesh.
-        filter.sharedMesh = MeshGenerator.GenerateIcoSphereMesh(recursionLevel, radius);
+    // Assign the material to the mesh.
+    renderer.material = material;
+  }
 
-        // Assign the material to the mesh.
-        renderer.material = material;
-    }
+  void OnDrawGizmos()
+  {
+    Gizmos.color = new Color(0, 0, 0, 0.5f);
+    Mesh mesh = MeshGenerator.GenerateIcoSphereMesh(recursionLevel, radius);
+    Gizmos.DrawWireMesh(mesh, 0, transform.position, Quaternion.identity, Vector3.one * radius);
+  }
 }
