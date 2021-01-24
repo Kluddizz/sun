d8adc4c67d01426370cd36d69a3ade135c5d319b
Julian <julian-k69@mailbox.org>
2021-01-24 15:29:05 +0100

    added delay to particle spawn

--- Assets/Scripts/FlareSpawner.cs
+++ Assets/Scripts/FlareSpawner.cs
@@ -8,26 +8,36 @@ public class FlareSpawner : MonoBehaviour
 {
   public int numberFlares = 10;
   public float radius = 1.0f;
+  public float delaySeconds;
+  public float skipDelayForParticles;
   public VisualEffect flareEffect;
 
   private List<Vector3> positions = new List<Vector3>();
 
   void Start()
   {
-    positions = FibonacciSphere.GeneratePoints(numberFlares, radius);
-
-    foreach (Vector3 p in positions)
-    {
-      Quaternion rotation = Quaternion.LookRotation(p, Vector3.up);
-      VisualEffect effect = Object.Instantiate<VisualEffect>(flareEffect, p, rotation);
-
-      new Thread(() => {
-        Thread.Sleep(5000);
-        effect.Play();
-      }).Start();
-    }
+        delaySeconds = 0.1f;
+        skipDelayForParticles = 50;
+        StartCoroutine(SpawnParticles());
   }
 
+  IEnumerator SpawnParticles()
+    {
+        positions = FibonacciSphere.GeneratePoints(numberFlares, radius);
+        float currentFlares = 0f;
+
+        foreach (Vector3 p in positions)
+        {
+            currentFlares++;
+            Quaternion rotation = Quaternion.LookRotation(p, Vector3.up);
+            VisualEffect effect = Object.Instantiate<VisualEffect>(flareEffect, p, rotation);
+
+            if (currentFlares >= skipDelayForParticles)
+            {
+                yield return new WaitForSeconds(delaySeconds);
+            }
+        }
+    }
   void OnDrawGizmos()
   {
     List<Vector3> positions = new List<Vector3>();
